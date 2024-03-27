8726ef064a5604b9b26fd217f621507af445d249
michael <dikandumichael@gmail.com>
2024-03-27 07:50:38 +0100

    Maven project

--- src/main/java/services/UserServices.java
+++ src/main/java/services/UserServices.java
@@ -1,5 +1,12 @@
 package services;
 
-public interface UserServices {
+import michaelBlog.dtos.request.*;
 
+public interface UserServices {
+    void registerUser(RegisterRequest registerRequest);
+    void login(LoginRequest loginRequest);
+    void logout(String username);
+    void updateProfile(UpdateRequest updateRequest);
+    void deleteAccount(DeleteUserRequest deleteRequest);
+    long numberOfUsers();
 }
---
+++ src/main/java/services/UserServicesImpl.java
@@ -0,0 +1,35 @@
+package services;
+
+import michaelBlog.dtos.request.*;
+
+public class UserServicesImpl implements UserServices {
+    @Override
+    public void registerUser(RegisterRequest registerRequest) {
+
+    }
+
+    @Override
+    public void login(LoginRequest loginRequest) {
+
+    }
+
+    @Override
+    public void logout(String username) {
+
+    }
+
+    @Override
+    public void updateProfile(UpdateRequest updateRequest) {
+
+    }
+
+    @Override
+    public void deleteAccount(DeleteUserRequest deleteRequest) {
+
+    }
+
+    @Override
+    public long numberOfUsers() {
+        return 0;
+    }
+}
