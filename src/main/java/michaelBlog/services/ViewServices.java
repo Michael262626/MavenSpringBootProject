e1db4a9429d6b579080ec4d044672df773250ba1
michael <dikandumichael@gmail.com>
2024-03-27 22:51:32 +0100

    Maven project

--- src/main/java/michaelBlog/services/ViewServices.java
+++ src/main/java/michaelBlog/services/ViewServices.java
@@ -1,4 +1,4 @@
-package services;
+package michaelBlog.services;
 
 public interface ViewServices {
     long numberOfViews();
---
+++ src/main/java/michaelBlog/services/ViewServicesImpl.java
@@ -0,0 +1,8 @@
+package michaelBlog.services;
+
+public class ViewServicesImpl implements ViewServices{
+    @Override
+    public long numberOfViews() {
+        return 0;
+    }
+}
