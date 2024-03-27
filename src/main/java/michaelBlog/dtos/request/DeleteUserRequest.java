81d4464a8542d4e254b8da0cc76eb6e72626da5d
michael <dikandumichael@gmail.com>
2024-03-27 07:50:11 +0100

    Maven project

---
+++ src/main/java/michaelBlog/dtos/request/DeleteUserRequest.java
@@ -0,0 +1,6 @@
+package michaelBlog.dtos.request;
+
+public class DeleteUserRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/michaelBlog/dtos/request/LoginRequest.java
@@ -0,0 +1,7 @@
+package michaelBlog.dtos.request;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+}
