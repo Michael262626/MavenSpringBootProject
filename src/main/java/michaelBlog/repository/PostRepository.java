9d8296943a822da6a50e2fc5d2494ea0cf811085
michael <dikandumichael@gmail.com>
2024-03-27 06:51:40 +0100

    Maven project

--- src/main/java/michaelBlog/repository/PostRepository.java
+++ src/main/java/michaelBlog/repository/PostRepository.java
@@ -2,6 +2,8 @@ package michaelBlog.repository;
 
 import michaelBlog.data.model.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 }
---
+++ src/main/java/services/PostServices.java
@@ -0,0 +1,17 @@
+package services;
+
+import michaelBlog.data.model.Post;
+import michaelBlog.dtos.request.CreatePostRequest;
+import michaelBlog.dtos.request.DeleteRequest;
+import michaelBlog.dtos.request.EditPostRequest;
+import michaelBlog.dtos.request.RetrievePost;
+
+import java.util.List;
+
+public interface PostServices {
+    void createPost(CreatePostRequest createPostRequest);
+    void editPost(EditPostRequest editPostRequest);
+    void deletePost(DeleteRequest deleteRequest);
+    Post retrievePost(RetrievePost retrieveRequest);
+    List<Post> getPost(String title);
+}
