aec64708953ae0cad92323cf8fec1e2a33e64b41
michael <dikandumichael@gmail.com>
2024-03-27 06:52:49 +0100

    Maven project

--- src/main/java/michaelBlog/repository/ViewRepository.java
+++ src/main/java/michaelBlog/repository/ViewRepository.java
@@ -2,6 +2,8 @@ package michaelBlog.repository;
 
 import michaelBlog.data.model.View;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ViewRepository extends MongoRepository<View, String> {
 }
