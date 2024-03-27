1c425dd7ad1b0f74bde0027bdd8435fe34a53d07
michael <dikandumichael@gmail.com>
2024-03-27 07:36:04 +0100

    Maven project

--- src/main/java/michaelBlog/data/model/Post.java
+++ src/main/java/michaelBlog/data/model/Post.java
@@ -1,15 +1,19 @@
 package michaelBlog.data.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 @Data
+@Document
 public class Post {
     private String title;
     private LocalDateTime localDateTime = LocalDateTime.now();
     private List<Comment> comments = new ArrayList<>();
+    @Id
     private String id;
     private List<View> views = new ArrayList<>();
     private String content;
--- src/main/java/michaelBlog/dtos/request/EditPostRequest.java
+++ src/main/java/michaelBlog/dtos/request/EditPostRequest.java
@@ -1,4 +1,9 @@
 package michaelBlog.dtos.request;
 
+import lombok.Data;
+
+@Data
 public class EditPostRequest {
+    private String title;
+    private String content;
 }
