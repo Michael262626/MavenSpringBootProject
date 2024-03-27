bdf7ebd37cfe46445169cc72e6aa84329127228e
michael <dikandumichael@gmail.com>
2024-03-27 07:29:50 +0100

    Maven project

--- src/main/java/michaelBlog/data/model/Comment.java
+++ src/main/java/michaelBlog/data/model/Comment.java
@@ -2,8 +2,10 @@ package michaelBlog.data.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 public class Comment {
     @Id
     private String id;
--- src/main/java/services/CommentServices.java
+++ src/main/java/services/CommentServices.java
@@ -1,5 +1,18 @@
 package services;
 
+import michaelBlog.data.model.Comment;
+import michaelBlog.dtos.request.CreateCommentRequest;
+import michaelBlog.dtos.request.DeleteCommentRequest;
+import michaelBlog.dtos.request.EditCommentRequest;
+import michaelBlog.dtos.request.RetrieveCommentRequest;
+
+import java.util.List;
+
 public interface CommentServices {
-    void Comment(String content);
+    void createComment(CreateCommentRequest createCommentRequest);
+    void editComment(EditCommentRequest editCommentRequest);
+    void deleteComment(DeleteCommentRequest deleteCommentRequest);
+    void retrieveComment(RetrieveCommentRequest retrieveComment);
+    List<Comment> comments(String content);
+    long numberOfComments();
 }
