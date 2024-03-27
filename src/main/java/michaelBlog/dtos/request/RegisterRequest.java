20ff7552fdfab8883b166187f4a65fa12e9131bd
michael <dikandumichael@gmail.com>
2024-03-27 07:50:25 +0100

    Maven project

---
+++ src/main/java/michaelBlog/dtos/request/RegisterRequest.java
@@ -0,0 +1,9 @@
+package michaelBlog.dtos.request;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/michaelBlog/dtos/request/UpdateRequest.java
@@ -0,0 +1,6 @@
+package michaelBlog.dtos.request;
+
+public class UpdateRequest {
+    private String newName;
+    private String newPassword;
+}
