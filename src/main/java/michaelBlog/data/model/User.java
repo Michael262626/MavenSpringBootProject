ec0786a80bb9a4b12e796ac2b2460c4c46df9730
michael <dikandumichael@gmail.com>
2024-03-27 07:36:52 +0100

    Maven project

--- src/main/java/michaelBlog/data/model/User.java
+++ src/main/java/michaelBlog/data/model/User.java
@@ -2,14 +2,16 @@ package michaelBlog.data.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 @Data
+@Document
 public class User {
-    @Id
     private String firstname;
     private String lastname;
     private String password;
+    @Id
     private String id;
     private String userName;
     private List<Post> postList;
--- src/main/java/michaelBlog/data/model/View.java
+++ src/main/java/michaelBlog/data/model/View.java
@@ -1,9 +1,11 @@
 package michaelBlog.data.model;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 @Data
+@Document
 public class View {
     private LocalDateTime timeOfView = LocalDateTime.now();
     private User viewer;
