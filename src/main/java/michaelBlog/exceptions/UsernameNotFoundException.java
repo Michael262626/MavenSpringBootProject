eb6c5f09b1acdeb06ec465094a57083d5ee51c7f
michael <dikandumichael@gmail.com>
2024-04-01 12:21:21 +0100

    Maven project

---
+++ src/main/java/michaelBlog/exceptions/UsernameNotFoundException.java
@@ -0,0 +1,5 @@
+package michaelBlog.exceptions;
+
+public class UsernameNotFoundException extends RuntimeException{
+    public UsernameNotFoundException(String message){super(message);}
+}
--- src/main/java/michaelBlog/services/UserServices.java
+++ src/main/java/michaelBlog/services/UserServices.java
@@ -4,10 +4,13 @@ import michaelBlog.dtos.request.*;
 
 
 public interface UserServices {
-    void registerUser(RegisterRequest registerRequest);
+    RegisterUserResponse registerUser(RegisterRequest registerRequest);
     void login(LoginRequest loginRequest);
     void logout(String username);
     void updateProfile(UpdateRequest updateRequest);
     void deleteAccount(DeleteUserRequest deleteRequest);
     long numberOfUsers();
+    CreatePostResponse createPost(CreatePostRequest createPostRequest);
+    long numberOfPost();
+    DeletePostResponse deletePost(DeleteRequest deleteRequestRequest);
 }
